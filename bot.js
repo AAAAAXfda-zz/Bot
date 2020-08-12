f919505a669bdc5fa5c2bd5b8e6fc0999aa88c71
AAAAAXfda <65740278+AAAAAXfda@users.noreply.github.com>
2020-08-12 16:51:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
