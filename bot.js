771c930bce65571e515707c013166ad94f09d9db
AAAAAXfda <65740278+AAAAAXfda@users.noreply.github.com>
2020-09-05 10:34:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const command = args.shift().toLocaleLowerCase();
+if (command === '8') {
+  const testEmbed = new Discord.RichEmbed()
+    .setColor(34a8eb)
+    .setTitle('Rules')
+    .setDescription('Rules for Jessology Clothing')
+  }
+})
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
