f4236d9e50749d6791786588e2bb365e95d58cf7
AAAAAXfda <65740278+AAAAAXfda@users.noreply.github.com>
2020-09-05 10:38:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const command = args.shift().toLocaleLowerCase();
-if (command === "8") {
-  const testEmbed = new Discord.RichEmbed()
-    .setColor(34a8eb)
-    .setTitle('Rules')
-    .setDescription('Rules for Jessology Clothing')
-  }
-})
+const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+channel.send(exampleEmbed);
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
