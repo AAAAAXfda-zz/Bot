414d6b093238921194198de8b04d96aebc0e3f5e
AAAAAXfda <65740278+AAAAAXfda@users.noreply.github.com>
2020-09-07 00:17:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,5 @@
-'use strict';
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-/**
- * An example of how you can send embeds
- */
-
-// Extract the required classes from the discord.js module
-const { Client, MessageEmbed } = require('discord.js');
-
-// Create an instance of a Discord client
-const client = new Client();
-
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-client.on('message', message => {
-  // If the message is "how to embed"
-  if (message.content === '5') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-    const embed = new MessageEmbed()
-      // Set the title of the field
-      .setTitle('A slick little embed')
-      // Set the color of the embed
-      .setColor(0xff0000)
-      // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!');
-    // Send the embed to the same channel as the message
-    message.channel.send(embed);
-  }
-});
-
-// Log our bot in using the token from https://discord.com/developers/applications
-client.login('NzQyNDk5NDc1OTQ3NTIwMDUw.XzHAhg.7MwdEztdNkuaOAPYcqjanmJWND4');
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
