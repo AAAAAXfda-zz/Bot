f6b49d2e2c67c78c051433e91860b1168cef6baa
AAAAAXfda <65740278+AAAAAXfda@users.noreply.github.com>
2020-09-07 00:19:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const { Client, MessageEmbed } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  // If the message is "how to embed"
+  if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+    const embed = new MessageEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xff0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
