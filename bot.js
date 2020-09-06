78499378443a16b8ab2eea4dccf806c68817989b
AAAAAXfda <65740278+AAAAAXfda@users.noreply.github.com>
2020-09-07 00:20:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
-const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const { Client, MessageEmbed } = require('discord.js');
-const client = new Client();
 
 client.on('ready', () => {
   console.log('I am ready!');
