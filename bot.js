69ee53d20bd7219fd0c44d74af0ea0179130adfe
AAAAAXfda <65740278+AAAAAXfda@users.noreply.github.com>
2020-12-16 15:09:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,21 @@
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-// THIS  MUST  BE  THIS  WAY
+client.on('ready', () => {
+  console.log(uuidv4());
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply(uuidv4());
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
